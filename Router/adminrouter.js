22caef33cd3d9f746fe22528f806ab526dcdcd25
harichandanavitasoft <harichandanavita@gmail.com>
2024-01-03 12:21:21 +0530

    admin routes completed

---
+++ Router/adminrouter.js
@@ -0,0 +1,23 @@
+const  express = require('express');
+const route = express.Router();
+const cors= require('cors');
+const admindata = require('../Model/adminmodel')
+ let corsoptions={
+    origin:['http://localhost:5000']
+ }
+route.post('/admin/create',cors(corsoptions),(req,res)=>{
+    const ad = new admindata(req.body)
+    ad.save();
+    res.status(201).json(ad)
+    
+});
+route.post('/admin/login',cors(corsoptions), async(req,res)=>{
+    const adminLogin = await admindata. findOne(req.body);
+    if(adminLogin){
+        res.status(201).json(adminLogin);
+      }else{
+        res.status(500).json('user login failed'); 
+      }
+})
+
+module.exports= route;
